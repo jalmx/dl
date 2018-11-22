56891e7314583d933019d1b36baeddd54ceac52a
Josef Leyva <jalm_x@hotmail.com>
2018-11-21 20:37:35 -0600

    production

--- web_dl/src/js/main.js
+++ web_dl/src/js/main.js
@@ -1,8 +1,8 @@
-import suma from './modules/suma'
-;
-((d)=>{
+const page = require('page');
 
-    const toogleMenu = ()=>{
+((d) => {
+
+    const toogleMenu = () => {
         const menu = d.querySelector('.nav__menu')
         menu.classList.toggle('active-menu')
     }
@@ -11,30 +11,35 @@ import suma from './modules/suma'
     btMenu.addEventListener('click', toogleMenu);
 
     const btBack = d.getElementById('back');
-    btBack.addEventListener('click',(e)=>{
+    btBack.addEventListener('click', (e) => {
         e.preventDefault();
         toogleMenu();
     });
-
-    
-    
 })(document);
 
 ((d) => {
     const banner = d.querySelector('.banner')
     const path = '../assets/img/full/full-'
-    
     let index = 1;
-    setInterval(()=>{
-      banner.style.backgroundImage = `url(${path}${index}.png)`
-      index++;
+    setInterval(() => {
+        banner.style.backgroundImage = `url(${path}${index}.png)`
+        index++;
+        if (index == 21) index = 1;
+    }, 3000);
+})(document);
 
-      if(index == 21) index = 1;
-  
-    },3000);
-  })(document);
 
-  (()=>{
-      console.log(suma(5,9));
-      
-  })();
+// const index = (ctx) => {
+//     const div = document.getElementById('mensaje');
+//     div.innerHTML = "mensaje nuevo";
+//     ctx.save();
+// }
+// const mensaje = (ctx) => {
+//     const div = document.getElementById('mensaje');
+//     div.innerText = "Prueba 2";
+//     ctx.save();
+// }
+
+// page('/', index);
+// page('/mensaje', mensaje);
+// page();
